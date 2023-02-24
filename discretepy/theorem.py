2150d90e0ac885497e938697b7e2d10672d53080
Clark Mattoon <mattoonc166@strose.edu>
2023-02-23 21:07:17 -0500

    removed redundancies in regards to theorems being immutable (property.setter that just raised errors)

--- discretepy/theorem.py
+++ discretepy/theorem.py
@@ -41,29 +41,9 @@ class Theorem:
     def queue(self):
         return self._queue
 
-    @queue.setter
-    def queue(self):
-        self._immutable_theorem_message("queue")
-
     @property
     def logical_comps(self):
         return self._logical_comps
-
-    @logical_comps.setter
-    def logical_comps(self):
-        self._immutable_theorem_message("logical_comps")
-
-    def _immutable_theorem_message(self, prop):
-        """
-        - a generic method that raises an error if an immutable property's setter method is called
-        - could be turned into a class/property decorator
-        :param prop: an immutable property name
-        :return: None
-        """
-
-        message = f"Theorem objects are immutable. If the '{prop}' property needs to be changed a new " \
-                  f"{self.__class__.__name__} should be created."
-        raise NotImplementedError(message)
 
     def __repr__(self):
         return f"{self.__class__.__name__}({self.logical_comps.__repr__()})"
